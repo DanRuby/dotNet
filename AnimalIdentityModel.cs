7414ed7932d45a6206d3bb58aae8ba4b1dbb537a
DanRuby <47368133+DanRuby@users.noreply.github.com>
2021-02-26 14:46:48 +0300

    Add files via upload

---
+++ AnimalIdentityModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Contracts;
+
+namespace Domain.Models
+{
+    public class AnimalIdentityModel : IAnimalIdentity
+    {
+        public int Id { get; }
+
+        public AnimalIdentityModel(int id)
+        {
+            Id = id;
+        }
+    }
+}
